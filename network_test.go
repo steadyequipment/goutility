d1b59c61b0b13c234e9c1bb65ae07c46fb980452
Ian G <yo.ian.g@gmail.com>
2016-07-06 14:13:31 -0400

    rename package to goutility ala repo name

--- network_test.go
+++ network_test.go
@@ -1,11 +1,11 @@
-package utility
+package goutility
 
 import (
 	"testing"
 )
 
 func TestGetFirstNonLoopbackIP(t *testing.T) {
-	
+
 	ip, error := GetFirstNonLoopbackIP()
 	if (len(ip) > 0 && error != nil) || (len(ip) == 0 && error == nil) {
 		t.Fatal("Should be given invalid ip and error or valid ip and no error: ip(\"%s\") error(\"%s\")", ip, error)
--- string_test.go
+++ string_test.go
@@ -1,4 +1,4 @@
-package utility
+package goutility
 
 import (
 	"testing"
